c89db31eeadcfe2f07b41bc85c91faa9d8861197
mxsharjeel <ksharjeel671@gmail.com>
2023-12-20 09:59:37 +0500

    Initial Commit

---
+++ src/cafeshopmanagementsystem/CafeShopManagementSystem.java
@@ -0,0 +1,27 @@
+package cafeshopmanagementsystem;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CafeShopManagementSystem extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setTitle("Cafe Shop Management System");
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
